d6d69eb259276a67ac6efd1762481a1c9299a10f
Mathieu Guigue <16764756+guiguem@users.noreply.github.com>
2024-09-13 15:06:43 +0000

    fasterac: add 2.20.1 bottle.

--- Formula/fasterac.rb
+++ Formula/fasterac.rb
@@ -5,6 +5,12 @@ class Fasterac < Formula
   sha256 "f15a41b2fb41ad417ed20126c2f93178aa526f526932c29c82b8064245be42a4"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/guiguem/homebrew-tap/releases/download/fasterac-2.20.1"
+    sha256 arm64_sonoma: "300a375bc6acfede59b212bfba54c957a9562fdf0c4dcd8afa995dc0c1d46145"
+    sha256 x86_64_linux: "2e2fd835377a023289515d6be434e933a2a3e59aaedbbccd03f9943cb8441a96"
+  end
+
   depends_on "gcc" => :build
   depends_on "pkg-config" => :build
   depends_on "zlib"
