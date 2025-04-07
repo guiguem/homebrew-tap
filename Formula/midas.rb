3644aa693dac87f689ce640c4b3dbd45a4885966
Mathieu Guigue <mgguigue@gmail.com>
2025-04-07 12:08:24 +0200

    Update doc

--- Formula/midas.rb
+++ Formula/midas.rb
@@ -42,19 +42,11 @@ class Midas < Formula
          export MIDAS_EXPTAB=$HOME/online/exptab
       The exptab file (defined by $MIDAS_EXPTAB) can be produced for the first time via
          echo "myexpt $HOME/online $USER" > $MIDAS_EXPTAB
+      where myexpt is the name of your experiment.
     EOS
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test midas`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/odbedit", "--help"
   end
 end
