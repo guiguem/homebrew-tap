96cb7488f370c4100f6397c436082228acc7e09c
Mathieu Guigue <mathieu.guigue@lpnhe.in2p3.fr>
2026-01-21 23:31:28 +0100

    G4/WCsim WIP

--- Formula/wcsim.rb
+++ Formula/wcsim.rb
@@ -4,13 +4,15 @@
 class Wcsim < Formula
   desc "The WCSim GEANT4 application"
   homepage "https://github.com/WCSim/WCSim"
-  url "https://github.com/WCSim/WCSim/archive/refs/tags/v1.12.29.zip"
+  url "https://github.com/WCSim/WCSim.git",
+    tag:      "v1.12.29",
+    revision: "43b3dae41cdc6cdb6676956d44ae474b2fb88959"
   sha256 "91324149a0bceb96b87ee7b26c4b31b52a200b32d2cf7d89e61137095687bd4e"
   license "MIT"
 
   depends_on "cmake" => :build
   depends_on "gcc" => :build
-  depends_on "geant4"
+  depends_on "geant4@10.3"
   depends_on "root"
 
 
@@ -23,6 +25,10 @@ class Wcsim < Formula
   def install
     # Remove unrecognized options if they cause configure to fail
     # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
+
+    inreplace "src/WCSimRootGeom.cc", '#include "WCSimRootGeom.hh"', "#include \"WCSimRootGeom.hh\"\n#include \"TMath.h\""
+    # inreplace "CMakeLists.txt", "git describe --always --long --tags --dirty", "v1.12.29"
+
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
