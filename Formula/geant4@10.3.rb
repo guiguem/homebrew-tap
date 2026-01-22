a3433d8a6e13cd4d580d4b9762019c056f16e297
Mathieu Guigue <mgguigue@gmail.com>
2026-01-22 13:39:27 +0100

    Fix style

--- Formula/geant4@10.3.rb
+++ Formula/geant4@10.3.rb
@@ -8,8 +8,8 @@ class Geant4AT103 < Formula
   option "with-gdml", "Use GDML"
   option "with-usolids", "Use USolids (experimental)"
 
-  depends_on "glfw"
   depends_on "cmake"
+  depends_on "glfw"
   depends_on "libx11"
   depends_on "libxmu"
   depends_on "qt" => :optional
@@ -66,112 +66,109 @@ class Geant4AT103 < Formula
   end
 
   def install
-
        polyfill_path = buildpath/"g4_compat.h"
-    polyfill_path.write <<~EOS
-      #ifndef G4_COMPAT_H
-      #define G4_COMPAT_H
-      #if __cplusplus >= 201103L
-      #include <algorithm>
-      #include <functional>
-      #include <random>
-      #include <type_traits>
-      #include <iterator>
+       polyfill_path.write <<~EOS
+         #ifndef G4_COMPAT_H
+         #define G4_COMPAT_H
+         #if __cplusplus >= 201103L
+         #include <algorithm>
+         #include <functional>
+         #include <random>
+         #include <type_traits>
+         #include <iterator>
 
-      namespace std {
-        // 1. Re-inject binary_function and unary_function
-        template<typename _Arg1, typename _Arg2, typename _Result>
-        struct binary_function {
-          typedef _Arg1 first_argument_type;
-          typedef _Arg2 second_argument_type;
-          typedef _Result result_type;
-        };
+         namespace std {
+           // 1. Re-inject binary_function and unary_function
+           template<typename _Arg1, typename _Arg2, typename _Result>
+           struct binary_function {
+             typedef _Arg1 first_argument_type;
+             typedef _Arg2 second_argument_type;
+             typedef _Result result_type;
+           };
 
-        template<typename _Arg, typename _Result>
-        struct unary_function {
-          typedef _Arg argument_type;
-          typedef _Result result_type;
-        };
+           template<typename _Arg, typename _Result>
+           struct unary_function {
+             typedef _Arg argument_type;
+             typedef _Result result_type;
+           };
 
-        // 2. Re-inject bind2nd and bind1st
-        template<typename _Fn, typename _Tp>
-        inline auto bind2nd(const _Fn& __f, const _Tp& __x) {
-          return std::bind(__f, std::placeholders::_1, __x);
-        }
+           // 2. Re-inject bind2nd and bind1st
+           template<typename _Fn, typename _Tp>
+           inline auto bind2nd(const _Fn& __f, const _Tp& __x) {
+             return std::bind(__f, std::placeholders::_1, __x);
+           }
 
-        template<typename _Fn, typename _Tp>
-        inline auto bind1st(const _Fn& __f, const _Tp& __x) {
-          return std::bind(__f, __x, std::placeholders::_1);
-        }
+           template<typename _Fn, typename _Tp>
+           inline auto bind1st(const _Fn& __f, const _Tp& __x) {
+             return std::bind(__f, __x, std::placeholders::_1);
+           }
 
-        // 3. Re-inject mem_fun AND mem_fun_ref (0 and 1 argument versions)
-        // 0-argument versions (Non-const and Const)
-        template<typename _Ret, typename _Tp>
-        inline auto mem_fun(_Ret (_Tp::*__f)()) { return std::mem_fn(__f); }
-        template<typename _Ret, typename _Tp>
-        inline auto mem_fun_ref(_Ret (_Tp::*__f)()) { return std::mem_fn(__f); }
-        template<typename _Ret, typename _Tp>
-        inline auto mem_fun(_Ret (_Tp::*__f)() const) { return std::mem_fn(__f); }
-        template<typename _Ret, typename _Tp>
-        inline auto mem_fun_ref(_Ret (_Tp::*__f)() const) { return std::mem_fn(__f); }
+           // 3. Re-inject mem_fun AND mem_fun_ref (0 and 1 argument versions)
+           // 0-argument versions (Non-const and Const)
+           template<typename _Ret, typename _Tp>
+           inline auto mem_fun(_Ret (_Tp::*__f)()) { return std::mem_fn(__f); }
+           template<typename _Ret, typename _Tp>
+           inline auto mem_fun_ref(_Ret (_Tp::*__f)()) { return std::mem_fn(__f); }
+           template<typename _Ret, typename _Tp>
+           inline auto mem_fun(_Ret (_Tp::*__f)() const) { return std::mem_fn(__f); }
+           template<typename _Ret, typename _Tp>
+           inline auto mem_fun_ref(_Ret (_Tp::*__f)() const) { return std::mem_fn(__f); }
 
-        // 1-argument versions (This fixes the G4INCLCDPP error)
-        template<typename _Ret, typename _Tp, typename _Arg>
-        inline auto mem_fun(_Ret (_Tp::*__f)(_Arg)) { return std::mem_fn(__f); }
-        template<typename _Ret, typename _Tp, typename _Arg>
-        inline auto mem_fun_ref(_Ret (_Tp::*__f)(_Arg)) { return std::mem_fn(__f); }
-        template<typename _Ret, typename _Tp, typename _Arg>
-        inline auto mem_fun(_Ret (_Tp::*__f)(_Arg) const) { return std::mem_fn(__f); }
-        template<typename _Ret, typename _Tp, typename _Arg>
-        inline auto mem_fun_ref(_Ret (_Tp::*__f)(_Arg) const) { return std::mem_fn(__f); }
+           // 1-argument versions (This fixes the G4INCLCDPP error)
+           template<typename _Ret, typename _Tp, typename _Arg>
+           inline auto mem_fun(_Ret (_Tp::*__f)(_Arg)) { return std::mem_fn(__f); }
+           template<typename _Ret, typename _Tp, typename _Arg>
+           inline auto mem_fun_ref(_Ret (_Tp::*__f)(_Arg)) { return std::mem_fn(__f); }
+           template<typename _Ret, typename _Tp, typename _Arg>
+           inline auto mem_fun(_Ret (_Tp::*__f)(_Arg) const) { return std::mem_fn(__f); }
+           template<typename _Ret, typename _Tp, typename _Arg>
+           inline auto mem_fun_ref(_Ret (_Tp::*__f)(_Arg) const) { return std::mem_fn(__f); }
 
-        // 4. Bridge random_shuffle
-        template<typename _RAI>
-        inline void random_shuffle(_RAI __first, _RAI __last) {
-          std::random_device __rd; std::mt19937 __g(__rd());
-          std::shuffle(__first, __last, __g);
-        }
-        template<typename _RAI, typename _RNG>
-        inline void random_shuffle(_RAI __first, _RAI __last, _RNG&&) {
-          std::random_device __rd; std::mt19937 __g(__rd());
-          std::shuffle(__first, __last, __g);
-        }
+           // 4. Bridge random_shuffle
+           template<typename _RAI>
+           inline void random_shuffle(_RAI __first, _RAI __last) {
+             std::random_device __rd; std::mt19937 __g(__rd());
+             std::shuffle(__first, __last, __g);
+           }
+           template<typename _RAI, typename _RNG>
+           inline void random_shuffle(_RAI __first, _RAI __last, _RNG&&) {
+             std::random_device __rd; std::mt19937 __g(__rd());
+             std::shuffle(__first, __last, __g);
+           }
 
-        // 5. Add ptr_fun
-        template<typename _Arg, typename _Result>
-        inline auto ptr_fun(_Result (*__f)(_Arg)) { return std::function<_Result(_Arg)>(__f); }
-        template<typename _Arg1, typename _Arg2, typename _Result>
-        inline auto ptr_fun(_Result (*__f)(_Arg1, _Arg2)) { return std::function<_Result(_Arg1, _Arg2)>(__f); }
-      }
-      #endif
-      #endif
-    EOS
+           // 5. Add ptr_fun
+           template<typename _Arg, typename _Result>
+           inline auto ptr_fun(_Result (*__f)(_Arg)) { return std::function<_Result(_Arg)>(__f); }
+           template<typename _Arg1, typename _Arg2, typename _Result>
+           inline auto ptr_fun(_Result (*__f)(_Arg1, _Arg2)) { return std::function<_Result(_Arg1, _Arg2)>(__f); }
+         }
+         #endif
+         #endif
+       EOS
 
-    mkdir "geant-build" do
-      
-      args = %w[
-        ../
-        -DGEANT4_USE_OPENGL_X11=OFF
-        -DGEANT4_USE_RAYTRACER_X11=ON
-        -DGEANT4_BUILD_EXAMPLE=ON
-        -DGEANT4_INSTALL_DATA=OFF
-        -DGEANT4_BUILD_MULTITHREADED=OFF
-        -DCMAKE_POLICY_VERSION_MINIMUM=3.5
-        -DGEANT4_USE_SYSTEM_ZLIB=ON
-        -DCMAKE_CXX_STANDARD=17
-      ]
-      args << "-DCMAKE_CXX_FLAGS=-include #{polyfill_path}"
+       mkdir "geant-build" do
+         args = %w[
+           ../
+           -DGEANT4_USE_OPENGL_X11=OFF
+           -DGEANT4_USE_RAYTRACER_X11=ON
+           -DGEANT4_BUILD_EXAMPLE=ON
+           -DGEANT4_INSTALL_DATA=OFF
+           -DGEANT4_BUILD_MULTITHREADED=OFF
+           -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+           -DGEANT4_USE_SYSTEM_ZLIB=ON
+           -DCMAKE_CXX_STANDARD=17
+         ]
+         args << "-DCMAKE_CXX_FLAGS=-include #{polyfill_path}"
 
-      args << "-DGEANT4_USE_QT=ON" if build.with? "qt"
-      args << "-DGEANT4_USE_G3TOG4=ON" if build.with? "g3tog4"
-      args << "-DGEANT4_USE_GDML=ON" if build.with? "gdml"
-      args << "-DGEANT4_USE_USOLIDS=ON" if build.with? "usolids"
-      args.concat(std_cmake_args)
-      system "cmake", *args
-      system "make", "install"
-
-    end
-      (include/"Geant4").install polyfill_path
+         args << "-DGEANT4_USE_QT=ON" if build.with? "qt"
+         args << "-DGEANT4_USE_G3TOG4=ON" if build.with? "g3tog4"
+         args << "-DGEANT4_USE_GDML=ON" if build.with? "gdml"
+         args << "-DGEANT4_USE_USOLIDS=ON" if build.with? "usolids"
+         args.concat(std_cmake_args)
+         system "cmake", *args
+         system "make", "install"
+       end
+       (include/"Geant4").install polyfill_path
   end
 
   def post_install
@@ -181,11 +178,11 @@ class Geant4AT103 < Formula
 
     # Define the permanent path
     permanent_include = include/"Geant4/g4_compat.h"
-    
-    # We use a wildcard to find the temp path because the random string 
+
+    # We use a wildcard to find the temp path because the random string
     # (e.g., geant4A10.3-20260122-...) changes every time.
     # We look for files in 'bin' and 'lib/Geant4-10.3.3' (or similar)
-    
+
     # Fix geant4-config script
     if File.exist?(bin/"geant4-config")
       inreplace bin/"geant4-config", %r{/private/tmp/geant4.*g4_compat\.h}, permanent_include
@@ -207,7 +204,7 @@ class Geant4AT103 < Formula
       For bash users:
         . $(brew --prefix geant4@10.3)/bin/geant4.sh
       For zsh users:
-        pushd $(brew --prefix geant4@10.3) >/dev/null; . bin/geant4.sh; popd >/dev/null
+        pushd $(brew --prefix geant4@10.3)/bin >/dev/null; . geant4.sh; popd >/dev/null
       For csh/tcsh users:
         source $(brew --prefix geant4@10.3)/bin/geant4.csh
     EOS
